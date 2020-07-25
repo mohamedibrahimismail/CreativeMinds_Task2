06f52bad250fcc32da3b0621c41718a7dba0517a
mohamedibrahimismail <mieibrahim15@gmail.com>
2020-07-25 03:51:54 +0200

    first commit

--- app/src/main/java/com/example/creativeminds_second_task/Constants.java
+++ app/src/main/java/com/example/creativeminds_second_task/Constants.java
@@ -1,6 +1,6 @@
 package com.example.creativeminds_second_task;
 
 public class Constants {
-    public static String TOKEN = "14506226e24bc087e39da5729cab747dfe5570f2";
+    public static String TOKEN = "WRITE_TOKEN_HERE";
     public static String  BASE_URL = "https://api.github.com";
 }
